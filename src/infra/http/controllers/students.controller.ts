9fdd69aada7df8056280c6ba9352b537aaea138a
luis maia <luismaia1407@gmail.com>
2023-05-26 13:00:27 -0300

    add return for test routes

--- src/infra/http/controllers/students.controller.ts
+++ src/infra/http/controllers/students.controller.ts
@@ -3,9 +3,9 @@ import { DeleteStudent } from '@application/use-cases/student/delete-student';
 import { FindStudent } from '@application/use-cases/student/find-student';
 import { ListStudents } from '@application/use-cases/student/list-students';
 import { UpdateStudent } from '@application/use-cases/student/update-student';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiProperty, ApiResponse } from '@nestjs/swagger';
-import { CreateStudentBody } from '../dto/student/create-student.dto';
+import { makeStudent } from '@test/factories/student-factory';
 import { StudentHttp } from '../types-class-http/student-http';
 import { StudentViewModel } from '../view-models/student-view-model';
 
@@ -29,7 +29,11 @@ export class StudentsController {
   @Get()
   @ApiResponse({ type: StudentHttp, isArray: true })
   async listAllStudents() {
-    const { students } = await this.listStudents.execute();
+    // const { students } = await this.listStudents.execute();
+    const students = [
+      makeStudent({ name: 'Estudante1', registration: '439990' }),
+      makeStudent({ name: 'Estudante2', registration: '429490' }),
+    ];
 
     return {
       students: students.map(StudentViewModel.toHTTP),
@@ -39,25 +43,28 @@ export class StudentsController {
   @Get(':id')
   @ApiResponse({ type: ResponseStudent })
   async getStudent(@Param('id') id: string) {
-    const { student } = await this.findStudent.execute({ studentId: id });
-
+    // const { student } = await this.findStudent.execute({ studentId: id });
+    const student = makeStudent({
+      name: 'EstudanteExemplo',
+      registration: '411112',
+    });
     return {
       message: 'Estudante encontrado!',
       student: StudentViewModel.toHTTP(student),
     };
   }
 
-  @Post()
-  @ApiResponse({ type: ResponseStudent })
-  async postStudent(@Body() createStudentBody: CreateStudentBody) {
-    const { student } = await this.createStudent.execute(createStudentBody);
+  // @Post()
+  // @ApiResponse({ type: ResponseStudent })
+  // async postStudent(@Body() createStudentBody: CreateStudentBody) {
+  //   const { student } = await this.createStudent.execute(createStudentBody);
 
-    return {
-      message: 'Estudante criado!',
+  //   return {
+  //     message: 'Estudante criado!',
 
-      student: StudentViewModel.toHTTP(student),
-    };
-  }
+  //     student: StudentViewModel.toHTTP(student),
+  //   };
+  // }
 
   // @Patch(':id')
   // @ApiResponse({ type: ResponseStudent })
